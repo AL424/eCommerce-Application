91bfebdb151ef97fcb11a437d790cd6b7c76a7c1
AL424 <algr5113@gmail.com>
2023-08-12 12:59:37 +0300

    feat: add client singin, singup

--- src/services/eCommerceService/BuildClient.ts
+++ src/services/eCommerceService/BuildClient.ts
@@ -10,11 +10,11 @@ const authMiddlewareOptions: AuthMiddlewareOptions = {
   host: 'https://auth.europe-west1.gcp.commercetools.com',
   projectKey: 'ecommerce-app-simple-team',
   credentials: {
-    clientId: 'n-XA7QIpASddTvTixm1jHbFv',
-    clientSecret: 'd26xmA2yRbkZoZUFgcPyZ3KELDnbWawD'
+    clientId: 'kgmMd3dEWi1v3jKD5-fXpLfH',
+    clientSecret: 'WljRUbWTYt31lEnrTIta0nMtIM_dPJlX'
   },
   scopes: [
-    'manage_my_quote_requests:ecommerce-app-simple-team manage_my_profile:ecommerce-app-simple-team view_published_products:ecommerce-app-simple-team manage_my_payments:ecommerce-app-simple-team create_anonymous_token:ecommerce-app-simple-team manage_my_orders:ecommerce-app-simple-team manage_my_quotes:ecommerce-app-simple-team view_categories:ecommerce-app-simple-team manage_my_shopping_lists:ecommerce-app-simple-team manage_my_business_units:ecommerce-app-simple-team'
+    'manage_customers:ecommerce-app-simple-team manage_my_quote_requests:ecommerce-app-simple-team manage_my_profile:ecommerce-app-simple-team view_published_products:ecommerce-app-simple-team manage_my_payments:ecommerce-app-simple-team create_anonymous_token:ecommerce-app-simple-team manage_my_orders:ecommerce-app-simple-team manage_my_quotes:ecommerce-app-simple-team view_categories:ecommerce-app-simple-team manage_my_shopping_lists:ecommerce-app-simple-team manage_my_business_units:ecommerce-app-simple-team'
   ],
   fetch
 };
--- src/services/eCommerceService/Client.ts
+++ src/services/eCommerceService/Client.ts
@@ -1,5 +1,9 @@
 import { ctpClient } from './BuildClient';
-import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import {
+  CustomerDraft,
+  CustomerSignin,
+  createApiBuilderFromCtpClient
+} from '@commercetools/platform-sdk';
 
 // Create apiRoot from the imported ClientBuilder and include your Project key
 const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
@@ -9,6 +13,40 @@ const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
 // Example call to return Product list
 export const getProduct = () => {
   return apiRoot.productProjections().get().execute();
+};
+
+// singin
+// Добавить функцию для выполнения входа в систему
+export const singin = (dataCustomer: CustomerSignin) => {
+  apiRoot
+    .login()
+    .post({
+      body: dataCustomer
+    })
+    .execute()
+    .then((resp) => resp.body)
+    .then((data) =>
+      console.log(
+        `Welcome, ${data.customer.firstName} ${data.customer.lastName}!!!`
+      )
+    )
+    .catch(() => console.log('Wrong e-mail or password'));
+};
+
+// create user
+// Добавить функцию для выполнения входа в систему
+export const singup = (dataCustomer: CustomerDraft) => {
+  apiRoot
+    .customers()
+    .post({ body: dataCustomer })
+    .execute()
+    .then((resp) => resp.body)
+    .then((data) =>
+      console.log(
+        `Welcome, ${data.customer.firstName} ${data.customer.lastName}!!!`
+      )
+    )
+    .catch(() => console.log('User not registered'));
 };
 
 // getProduct()
