ef1b2b6aef35622a7a414ee4869b9a830995d7f9
AL424 <algr5113@gmail.com>
2023-08-16 00:24:11 +0300

    feat: add Local Storage service

--- src/services/localStorage/LocalStorage.service.tsx
+++ src/services/localStorage/LocalStorage.service.tsx
@@ -1,1 +1,14 @@
-export {};
+export const LocalStorage = {
+  get(key: string) {
+    return localStorage.getItem(key);
+  },
+  set(key: string, value: string) {
+    localStorage.setItem(key, value);
+  },
+  remove(key: string) {
+    localStorage.remove(key);
+  },
+  clear() {
+    localStorage.clear();
+  }
+};
