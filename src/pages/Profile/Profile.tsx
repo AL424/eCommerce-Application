63ffb0ea6265cbb546fef865ed9965924a48c5ae
AL424 <algr5113@gmail.com>
2023-08-25 16:46:07 +0300

    feat: update personal info in profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,11 +1,28 @@
-import React, { ReactElement } from 'react';
+import './Profile.scss';
+import React, { useState, useEffect, ReactElement } from 'react';
 import { PersonalInfo } from '../../components/forms/PersonalInfo/PersonalInfo';
+import { Customer } from '@commercetools/platform-sdk';
+import { getMe } from '../../services/eCommerceService/Customer';
+import { getApiRoot } from '../../services/eCommerceService/ApiRoot';
 
 export const Profile = (): ReactElement => {
+  const [customer, setCustomer] = useState({} as Customer);
+
+  const getCustomer = async () => {
+    const resp = await getMe(getApiRoot());
+    setCustomer(resp);
+  };
+
+  useEffect(() => {
+    getCustomer();
+  }, []);
+
   return (
     <div className="profile-page">
       <h1>Profile</h1>
-      <PersonalInfo />
+      <form className="form-container">
+        <PersonalInfo customer={customer} />
+      </form>
     </div>
   );
 };
