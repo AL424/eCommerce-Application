3517c8911beb51b7f8b8cb1eac7a0f02bc2cec16
AL424 <algr5113@gmail.com>
2023-08-25 16:44:09 +0300

    feat: display personal info, add edit mode

--- src/components/forms/PersonalInfo/PersonalInfo.tsx
+++ src/components/forms/PersonalInfo/PersonalInfo.tsx
@@ -1,18 +1,84 @@
+import './PersonalInfo.scss';
 import React, { useState } from 'react';
+import { Customer } from '@commercetools/platform-sdk';
+import Input from '../../common/Input/Input';
+import { useForm } from 'react-hook-form';
+import { emailValidation } from '../../../utils/validation/emailValidation';
+import { nameValidation } from '../../../utils/validation/nameValidation';
+// import { ageValidation } from '../../../utils/validation/ageValidation';
 
-export const PersonalInfo: React.FC = () => {
-  const [disabled, setDisabled] = useState(true);
-  const setEditMode = () => {
-    if (disabled) setDisabled(false);
-    setDisabled(true);
-  };
+interface PersonalInfoProps {
+  customer: Customer;
+}
 
-  // const [customer, setCustomer] = useState();
+interface PersonalInfoChange {
+  email: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+}
+
+export const PersonalInfo: React.FC<PersonalInfoProps> = ({ customer }) => {
+  const [editmode, setEditmode] = useState(false);
+  const {
+    register,
+    formState: { errors }
+  } = useForm<PersonalInfoChange>();
 
   return (
-    <form>
-      <fieldset disabled={disabled}></fieldset>
-      <button onClick={() => setEditMode()}>Edit</button>
-    </form>
+    <>
+      <fieldset disabled={!editmode} className="personal-info">
+        <legend>Personal Information</legend>
+        <Input
+          label="E-mail"
+          placeholder="email@example.com"
+          inputProps={register('email', emailValidation)}
+          error={errors.email}
+          defaultValue={customer.email}
+        />
+
+        <Input
+          label="Name"
+          placeholder="John"
+          inputProps={register('firstName', nameValidation)}
+          error={errors.firstName}
+          defaultValue={customer.firstName}
+        />
+
+        <Input
+          label="Surname"
+          placeholder="Doe"
+          inputProps={register('lastName', {
+            ...nameValidation,
+            required: 'Surname is required'
+          })}
+          error={errors.lastName}
+          defaultValue={customer.lastName}
+        />
+
+        {/* Разобраться с валидацией даты рождения */}
+        <Input
+          label="Date of Birth"
+          type="date"
+          inputProps={register('dateOfBirth')}
+          error={errors.dateOfBirth}
+          defaultValue={customer.dateOfBirth}
+        />
+      </fieldset>
+      <div className="button-wrap">
+        <button
+          type="button"
+          className={editmode ? 'button button_cancel' : 'button'}
+          onClick={() => setEditmode((prev) => !prev)}
+        >
+          {editmode ? 'Cancel' : 'Edit'}
+        </button>
+        {editmode && (
+          <button type="button" className="button button_save">
+            Save
+          </button>
+        )}
+      </div>
+    </>
   );
 };
