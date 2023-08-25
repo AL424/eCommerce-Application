718bd9a6864710f1b48a85f3d8c94319a8c6acef
Egor <itpd2010@yandex.ru>
2023-08-25 17:27:29 +0300

    feat: slider settings

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { getProductById } from '../../services/eCommerceService/Client';
 import { ProductData } from '@commercetools/platform-sdk';
-import Slider from 'react-slick';
+import Slider, { Settings } from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const id = '12236346-a8dd-40b5-ba11-6077e197f5e0';
-// const id2 = '30eb4525-39a5-4982-b4ab-9b0ea5c7c5a1';
+// const id = '30eb4525-39a5-4982-b4ab-9b0ea5c7c5a1';
 
 export const ProductDetails = () => {
   const [product, setProduct] = useState<ProductData | null>(null);
+
+  const largeSliderRef = useRef<Slider | null>(null);
+  const smallSliderRef = useRef<Slider | null>(null);
 
   useEffect(() => {
     getProductById(id)
@@ -19,6 +24,30 @@ export const ProductDetails = () => {
         console.error('Error occured:', error);
       });
   }, []);
+
+  const largeSliderSettings: Settings = {
+    dots: false,
+    infinite: true,
+    arrows: true,
+    // dotsClass: 'slick-dots slick-thumb',
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: '.largeSlider',
+    asNavFor: smallSliderRef.current || undefined
+  };
+
+  const smallSliderSettings: Settings = {
+    dots: false,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    className: '.smallSlider',
+    asNavFor: largeSliderRef.current || undefined
+  };
 
   // const transformDetails = (data: ProductData) => {
   //   return {
@@ -52,14 +81,43 @@ export const ProductDetails = () => {
               Price: {product.masterVariant.prices?.[0].value.centAmount}{' '}
               {product.masterVariant.prices?.[0].value.currencyCode}
             </p>
-            {product.masterVariant.images?.map((image, index) => (
+            {/* {product.masterVariant.images?.map((image, index) => (
               <img
                 key={index}
                 src={image.url}
                 alt={image.label}
                 // style={{ width: '350px', height: '350px' }}
               />
-            ))}
+            ))} */}
+
+            <Slider
+              {...largeSliderSettings}
+              ref={(slider) => (largeSliderRef.current = slider)}
+            >
+              {product.masterVariant.images?.map((image, index) => (
+                <div key={index}>
+                  <img
+                    src={image.url}
+                    alt={image.label}
+                    style={{ width: '350px', height: '350px' }}
+                  />
+                </div>
+              ))}
+            </Slider>
+            <Slider
+              {...smallSliderSettings}
+              ref={(slider) => (smallSliderRef.current = slider)}
+            >
+              {product.masterVariant.images?.map((image, index) => (
+                <div key={index}>
+                  <img
+                    src={image.url}
+                    alt={image.label}
+                    style={{ width: '100px', height: '100px' }}
+                  />
+                </div>
+              ))}
+            </Slider>
           </div>
           {/* {product.variants.map((variant) => (
             <div key={variant.id}>
