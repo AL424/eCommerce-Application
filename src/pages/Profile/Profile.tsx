632ace90f160674104d5b0977caea0b25cc6e832
AL424 <algr5113@gmail.com>
2023-08-26 10:55:14 +0300

    feat: display addresses

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,6 +5,7 @@ import { Customer } from '@commercetools/platform-sdk';
 import { getMe } from '../../services/eCommerceService/Customer';
 import { getApiRoot } from '../../services/eCommerceService/ApiRoot';
 import { PasswordInfo } from '../../components/forms/PasswordInfo/PaswordInfo';
+import { AddressesInfo } from '../../components/forms/AddressesInfo/AddressesInfo';
 
 export const Profile = (): ReactElement => {
   const [customer, setCustomer] = useState({} as Customer);
@@ -24,6 +25,7 @@ export const Profile = (): ReactElement => {
       <form className="form-container profile-page__form">
         <PersonalInfo customer={customer} />
         <PasswordInfo />
+        <AddressesInfo customer={customer} />
       </form>
     </div>
   );
