39a343b0c5473a08eca120b29c1b6344ed4ec85e
Rimfis <dimitri.rim@yandex.ru>
2023-08-27 20:14:39 +0300

    feat: add range component

---
+++ src/components/common/Range/Range.tsx
@@ -0,0 +1,97 @@
+import {
+  ChangeEvent,
+  FC,
+  useCallback,
+  useEffect,
+  useState,
+  useRef
+} from 'react';
+import './Range.scss';
+import React from 'react';
+
+interface MultiRangeSliderProps {
+  min: number;
+  max: number;
+  onChange: ({ min, max }: { min: number; max: number }) => void;
+}
+
+export const Range: FC<MultiRangeSliderProps> = ({ min, max, onChange }) => {
+  const [minVal, setMinVal] = useState(min);
+  const [maxVal, setMaxVal] = useState(max);
+  const minValRef = useRef<HTMLInputElement>(null);
+  const maxValRef = useRef<HTMLInputElement>(null);
+  const range = useRef<HTMLDivElement>(null);
+
+  const getPercent = useCallback(
+    (value: number) => Math.round(((value - min) / (max - min)) * 100),
+    [min, max]
+  );
+
+  useEffect(() => {
+    if (maxValRef.current) {
+      const minPercent = getPercent(minVal);
+      const maxPercent = getPercent(+maxValRef.current.value);
+
+      if (range.current) {
+        range.current.style.left = `${minPercent}%`;
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }
+  }, [minVal, getPercent]);
+
+  useEffect(() => {
+    if (minValRef.current) {
+      const minPercent = getPercent(+minValRef.current.value);
+      const maxPercent = getPercent(maxVal);
+
+      if (range.current) {
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }
+  }, [maxVal, getPercent]);
+
+  useEffect(() => {
+    onChange({ min: minVal, max: maxVal });
+  }, [minVal, maxVal, onChange]);
+
+  return (
+    <div className="slider-range__container">
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={minVal}
+        ref={minValRef}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          const value = Math.min(+event.target.value, maxVal - 1);
+          setMinVal(value);
+          event.target.value = value.toString();
+        }}
+        className={`${'thumb thumb__zindex-3'} ${
+          minVal > max - 100 ? 'thumb__zindex-5' : ''
+        }`}
+      />
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={maxVal}
+        ref={maxValRef}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          const value = Math.max(+event.target.value, minVal + 1);
+          setMaxVal(value);
+          event.target.value = value.toString();
+        }}
+        className="thumb thumb__zindex-4"
+      />
+      <div className="slider-range">
+        <div className="slider-range__track"></div>
+        <div ref={range} className="slider-range__range"></div>
+      </div>
+      <div className="slider-range__result">
+        <div>{minVal}</div>
+        <div>{maxVal}</div>
+      </div>
+    </div>
+  );
+};
