379c7724526030b0ac223aa2afe3c6a5c03b08c8
Egor <itpd2010@yandex.ru>
2023-08-31 10:07:50 +0300

    fix: delete function check

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.scss';
 import App from './App';
 import store from './services/store/store';
 import { Provider } from 'react-redux';
-import { getProductById } from './services/eCommerceService/Client';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,7 +15,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-const id = '12236346-a8dd-40b5-ba11-6077e197f5e0';
-const res = getProductById(id);
-console.log(res);
