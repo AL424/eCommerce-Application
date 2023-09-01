0522ee44e0f414ce52f116c90a70aaf8265f2d7a
AL424 <algr5113@gmail.com>
2023-09-01 20:27:31 +0300

    feat: update address validation

--- src/utils/validation/streetValidation.ts
+++ src/utils/validation/streetValidation.ts
@@ -1,7 +1,16 @@
 export const streetValidation = {
-  required: 'Must contain at least one character',
-  minLength: {
-    value: 1,
-    message: 'Must contain at least one character'
+  required: 'Street is required',
+  validate(value: string | undefined) {
+    if (!value) return;
+
+    if (value.trim() !== value) {
+      return 'Street should not have leading or trailing spaces';
+    }
+
+    if (!/^[\d\w\s\.\-]*$/.test(value)) {
+      return 'Use letters, digits, spaces, and symbols . -';
+    }
+
+    return true;
   }
 };
