e258ae3693ecabb948b60530feb966bb079961c2
Egor <itpd2010@yandex.ru>
2023-09-07 15:24:48 +0300

    feat: add cart button to cards

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,6 +3,7 @@ import './ProductCard.scss';
 import { ProductProjection } from '@commercetools/platform-sdk';
 import { Link } from 'react-router-dom';
 import { Route } from '../../Router/Router';
+import { Button } from '../buttons/button';
 
 const containerClass = 'card';
 const productImageClass = 'card__img';
@@ -11,6 +12,7 @@ const titleClass = 'card__title';
 const priceClass = 'card__price';
 const oldPriceClass = 'card__price-old';
 const descpiptionClass = 'card__description';
+const buttonContainer = 'card__addButton';
 
 export const ProductCard: React.FC<{ data: ProductProjection }> = ({
   data
@@ -19,6 +21,11 @@ export const ProductCard: React.FC<{ data: ProductProjection }> = ({
     ru: 'ru',
     en: 'en-US'
   };
+
+  const handleButtonClick = () => {
+    console.log('Added to Card');
+  };
+
   const imgUrl = data.masterVariant.images;
   const title = data.name ? data.name[languages.en] : '';
   const decription = data.description;
@@ -31,25 +38,39 @@ export const ProductCard: React.FC<{ data: ProductProjection }> = ({
     : null;
 
   return (
-    <Link className={containerClass} to={`${Route.product}/${data.id}`}>
-      <div className={productImageClass}>
-        <img
-          src={
-            imgUrl instanceof Array && imgUrl?.length > 0 ? imgUrl[0].url : ''
+    <>
+      {/* <Link className={containerClass} to={`${Route.product}/${data.id}` }> */}
+      <Link
+        className={containerClass}
+        to={`${Route.product}/${data.id}`}
+        onClick={(e) => {
+          if (e.target instanceof HTMLButtonElement) {
+            e.preventDefault();
           }
-          alt={title}
-        />
-      </div>
-      <div className={textContainerClass}>
-        <h4 className={titleClass}>{title}</h4>
-        <p className={priceClass}>
-          <span className={discond ? oldPriceClass : ''}>{price}</span>
-          {discond && <span>{discond}</span>}
-        </p>
-        <p className={descpiptionClass}>
-          {decription && decription[languages.en]}
-        </p>
-      </div>
-    </Link>
+        }}
+      >
+        <div className={productImageClass}>
+          <img
+            src={
+              imgUrl instanceof Array && imgUrl?.length > 0 ? imgUrl[0].url : ''
+            }
+            alt={title}
+          />
+        </div>
+        <div className={textContainerClass}>
+          <h4 className={titleClass}>{title}</h4>
+          <div className={buttonContainer}>
+            <p className={priceClass}>
+              <span className={discond ? oldPriceClass : ''}>{price}</span>
+              {discond && <span>{discond}</span>}
+            </p>
+            <Button title={'🛒'} onClick={handleButtonClick} />
+          </div>
+          <p className={descpiptionClass}>
+            {decription && decription[languages.en]}
+          </p>
+        </div>
+      </Link>
+    </>
   );
 };
