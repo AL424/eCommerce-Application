6024e4372335c5df41e4f9d10b334fef469a3875
Rimfis <dimitri.rim@yandex.ru>
2023-09-11 20:41:27 +0300

    feat: getting a previously created bucket

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Router/Router';
+import { getCartById, getMyActiveCart } from './services/eCommerceService/Cart';
+import { useAppDispatch } from './services/store/hooks';
+import { setCartData } from './services/store/cartSlice';
+import { Cart } from '@commercetools/platform-sdk';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const cartId = localStorage.getItem('cart-id');
+    const customerId = localStorage.getItem('customer-id');
+    const setData = (data: Cart | string) => {
+      if (typeof data !== 'string') dispatch(setCartData(data));
+    };
+
+    if (!customerId && cartId) getCartById(cartId).then(setData);
+    if (customerId) getMyActiveCart().then(setData);
+  }, [dispatch]);
+
   return (
     <div className="App">
       <RouterProvider router={router} />
