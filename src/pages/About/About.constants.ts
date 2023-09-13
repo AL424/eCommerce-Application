02fde76ac0c41b928a946a33084e16e4a46163e3
Egor <itpd2010@yandex.ru>
2023-09-13 11:21:16 +0300

    refactor: Same type data are moved into constants

---
+++ src/pages/About/About.constants.ts
@@ -0,0 +1,62 @@
+import michaelScottImg from '../../assets/michaelscott.jpg';
+import jimHalpertImg from '../../assets/jimhalpert.jpg';
+import dwightschruteImg from '../../assets/dwightschrute.jpg';
+
+const teamMembersData = [
+  {
+    name: 'Alexander',
+    role: 'Frontend-Developer, Team Lead',
+    bio: "Alexander is our visionary leader and chief problem solver. With a passion for innovation and a knack for solving complex technical challenges, he's the driving force behind our projects. Alexander's leadership skills have been instrumental in our team's success",
+    img: michaelScottImg,
+    githubLink: 'https://github.com/al424'
+  },
+  {
+    name: 'Dmitriy',
+    role: 'Frontend-Developer',
+    bio: "Dmitriy is a frontend wizard who turns design concepts into stunning user interfaces. His attention to detail and commitment to creating user-friendly experiences are unmatched. Dmitriy's extensive experience in web development have been instrumental in our team's success",
+    img: jimHalpertImg,
+    githubLink: 'https://github.com/dmitriyrim'
+  },
+  {
+    name: 'Egor Berezhnov',
+    role: 'Frontend-Developer',
+    bio: "Egor is the mastermind behind our coordinated efforts. He has successfully assembled and led our team to successful completion of the project. Egor ensures everyone is in sync, and helps coordinate our actions. His commitment to teamwork and communication has been a driving force in our project's success.",
+    img: dwightschruteImg,
+    githubLink: 'https://github.com/ygrcore'
+  }
+];
+
+export const teamTasksData = [
+  {
+    name: 'Alexander',
+    tasks: [
+      'Repository Setup',
+      'Task Board Setup',
+      'CommerceTools Project and API Client Setup',
+      'User Profile Page Implementation',
+      'Basket Page Implementation'
+    ]
+  },
+  {
+    name: 'Dmitriy',
+    tasks: [
+      'Development Environment Configuration',
+      'Development Scripts',
+      'State Management, Automatic Login, and Redirection',
+      'Routing Implementation',
+      'Catalog Page Implementation'
+    ]
+  },
+  {
+    name: 'Egor Berezhnov',
+    tasks: [
+      'Structure and Organize a React Application',
+      'Login Page Implementation',
+      'Registration Page Implementation',
+      'Detailed Product Page Implementation',
+      'About Us Page Implementation'
+    ]
+  }
+];
+
+export default teamMembersData;
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Breadcrumb } from '../../components/LocationPages/Breadcrumb';
 import TeamMember from '../../components/common/TeamMember/TeamMember';
-import michaelScottImg from '../../assets/michaelscott.jpg';
-import jimHalpertImg from '../../assets/jimhalpert.jpg';
-import dwightschruteImg from '../../assets/dwightschrute.jpg';
+import teamMembersData from './About.constants';
+import { teamTasksData } from './About.constants';
 import rsslogo from '../../assets/rs_school-og.png';
 import './About.scss';
 
@@ -20,27 +19,16 @@ export const About = () => {
       <div>
         <h1>About Us</h1>
         <div className={teamCards}>
-          <TeamMember
-            name="Alexander"
-            role="Frontend-Developer, Team Lead"
-            bio="Alexander is our visionary leader and chief problem solver. With a passion for innovation and a knack for solving complex technical challenges, he's the driving force behind our projects. Alexander's leadership skills have been instrumental in our team's success"
-            img={michaelScottImg}
-            githubLink="https://github.com/al424"
-          />
-          <TeamMember
-            name="Dmitriy"
-            role="Frontend-Developer"
-            bio="Dmitriy is a frontend wizard who turns design concepts into stunning user interfaces. His attention to detail and commitment to creating user-friendly experiences are unmatched. Dmitriy's extensive experience in web development have been instrumental in our team's success"
-            img={jimHalpertImg}
-            githubLink="https://github.com/dmitriyrim"
-          />
-          <TeamMember
-            name="Egor Berezhnov"
-            role="Frontend-Developer"
-            bio="Egor the mastermind behind our coordinated efforts. He has successfully assembled and led our team to successful completion of the project. Egor ensures everyone is in sync, and helps coordinate our actions. His commitment to teamwork and communication has been a driving force in our project's success."
-            img={dwightschruteImg}
-            githubLink="https://github.com/ygrcore"
-          />
+          {teamMembersData.map((member, index) => (
+            <TeamMember
+              key={index}
+              name={member.name}
+              role={member.role}
+              bio={member.bio}
+              img={member.img}
+              githubLink={member.githubLink}
+            />
+          ))}
         </div>
         <div className={additionalInfo}>
           <h2>Our Contributions</h2>
@@ -49,36 +37,16 @@ export const About = () => {
             project. Here are some highlights of every our team member:
           </p>
           <div className={ourContribution}>
-            <div>
-              <h4>Alexander</h4>
-              <ul>
-                <li>Repository Setup</li>
-                <li>Task Board Setup</li>
-                <li>CommerceTools Project and API Client Setup</li>
-                <li>User Profile Page Implementation</li>
-                <li>Basket Page Implementation</li>
-              </ul>
-            </div>
-            <div>
-              <h4>Dmitriy</h4>
-              <ul>
-                <li>Development Environment Configuration</li>
-                <li>Development Scripts</li>
-                <li>State Management, Automatic Login, and Redirection</li>
-                <li>Routing Implementation</li>
-                <li>Catalog Page Implementation</li>
-              </ul>
-            </div>
-            <div>
-              <h4>Egor</h4>
-              <ul>
-                <li>Structure and Organize a React Application</li>
-                <li>Login Page Implementation</li>
-                <li>Registration Page Implementation</li>
-                <li>Detailed Product Page Implementation</li>
-                <li>About Us Page Implementation</li>
-              </ul>
-            </div>
+            {teamTasksData.map((memberTasks, index) => (
+              <div key={index}>
+                <h4>{memberTasks.name}</h4>
+                <ul>
+                  {memberTasks.tasks.map((task, taskIndex) => (
+                    <li key={taskIndex}>{task}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </div>
         </div>
         <h2>Effective Collaboration</h2>
