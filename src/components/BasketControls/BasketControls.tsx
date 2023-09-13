fd78a0221865864ae48a2efe73236abd145ff282
AL424 <algr5113@gmail.com>
2023-09-13 20:44:52 +0300

    feat: BasketControls add buttons class name

--- src/components/BasketControls/BasketControls.tsx
+++ src/components/BasketControls/BasketControls.tsx
@@ -1,6 +1,6 @@
 import './BasketControls.scss';
 import React, { useState, useEffect } from 'react';
-import { Button } from '../buttons/button';
+import { Button } from '../Button/Button';
 import { useAppDispatch, useAppSelector } from '../../services/store/hooks';
 import {
   createMyCart,
@@ -25,6 +25,13 @@ export const BasketControls: React.FC<Props> = ({ productId }) => {
   const [quantity, setQuantity] = useState(product?.quantity || 0);
   const cart = useAppSelector((state) => state.cartData.value);
   const dispatch = useAppDispatch();
+
+  const [btnClassList, setBtnClassList] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (quantity < 2) setBtnClassList(['not-active']);
+    else setBtnClassList([]);
+  }, [quantity]);
 
   useEffect(() => {
     if (!cart) setPdoductInCart(false);
@@ -117,16 +124,28 @@ export const BasketControls: React.FC<Props> = ({ productId }) => {
   return (
     <div className="basket-controls">
       {!productInCart && (
-        <Button title="Add to Basket" onClick={onAddToBasket} />
+        <Button
+          title="Add to Basket"
+          onClick={onAddToBasket}
+          classList={['button_add']}
+        />
       )}
       {productInCart && (
         <>
-          <div className="count">
-            <Button title="-" onClick={onTakeAwayQuantity} />
-            <span className="basket-controls__count">{quantity}</span>
+          <div className="count-wrap">
+            <Button
+              title="-"
+              onClick={onTakeAwayQuantity}
+              classList={btnClassList}
+            />
+            <span className="count">{quantity}</span>
             <Button title="+" onClick={onAddQuantity} />
           </div>
-          <Button title="Remove from Basket" onClick={onRemoveLineItem} />
+          <Button
+            title="Remove from Basket"
+            onClick={onRemoveLineItem}
+            classList={['button_remove']}
+          />
         </>
       )}
     </div>
