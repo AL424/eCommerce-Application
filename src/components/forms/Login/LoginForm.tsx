a1638391b425de62e7c7b836e871ee95dab1a85b
AL424 <algr5113@gmail.com>
2023-09-16 12:46:15 +0300

    fix: login message use toastify

--- src/components/forms/Login/LoginForm.tsx
+++ src/components/forms/Login/LoginForm.tsx
@@ -8,27 +8,19 @@ import PasswordField from '../../common/PasswordInput/PasswordInput';
 import { singin } from '../../../services/eCommerceService/Customer';
 import { CustomerSignin } from '@commercetools/platform-sdk';
 import { authOn } from '../../../services/store/authSlice';
-import { modalLoginOn } from '../../../services/store/modalLoginSlice';
 import { useAppDispatch } from '../../../services/store/hooks';
+import { toast } from 'react-toastify';
 
 const buttonClass = 'button';
 const inputClass = 'form-input';
 
 const LoginForm: React.FC = () => {
-  // const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [loginError, setLoginError] = useState(false);
   const [formDisabled, setFormDisabled] = useState(false);
   const dispatch = useAppDispatch();
-
-  // type LoginInputs = {
-  //   email: string;
-  //   password: string;
-  // };
 
   const {
     register,
     handleSubmit,
-    // watch,
     formState: { errors }
   } = useForm<CustomerSignin>({
     mode: 'onChange'
@@ -38,23 +30,13 @@ const LoginForm: React.FC = () => {
     setFormDisabled(true);
     const customer = await singin(data);
     if (typeof customer === 'string') {
-      setLoginError(true);
+      toast.error(customer);
       setFormDisabled(false);
     } else {
       dispatch(authOn());
-      dispatch(modalLoginOn());
+      toast.success(`${data.email} login successful`);
     }
   };
-
-  const onInput = () => {
-    setLoginError(false);
-  };
-
-  // const toggleConfirmPasswordVisibility = () => {
-  //   setShowConfirmPassword(!showConfirmPassword);
-  // };
-
-  // console.log(watch());
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="form-container">
@@ -64,7 +46,6 @@ const LoginForm: React.FC = () => {
         inputProps={register('email', emailValidation)}
         error={errors.email}
         formDisabled={formDisabled}
-        onInput={onInput}
         id="email"
       />
 
@@ -74,34 +55,8 @@ const LoginForm: React.FC = () => {
         inputProps={register('password', passwordValidation)}
         error={errors.password}
         formDisabled={formDisabled}
-        onInput={onInput}
         id="password"
       />
-
-      {/* <label>Confirm Password</label>
-        <div style={{ display: 'flex' }}>
-          <input
-            placeholder="Confirm Password"
-            type={showConfirmPassword ? 'text' : 'password'}
-            {...register('confirm_password', {
-              required: true,
-              validate: (val: string) => {
-                if (watch('password') != val) {
-                  return 'Your passwords do no match';
-                }
-              }
-            })}
-          />
-          <button type="button" onClick={toggleConfirmPasswordVisibility}>
-            {showConfirmPassword ? 'Hide' : 'Show'}
-          </button>
-        </div>
-
-        <p>
-          {errors.confirm_password && (
-            <span>{errors.confirm_password.message}</span>
-          )}
-        </p> */}
 
       <input
         className={`${inputClass} ${buttonClass}`}
@@ -109,10 +64,6 @@ const LoginForm: React.FC = () => {
         type="submit"
         disabled={formDisabled}
       />
-
-      <p className="error-message">
-        {loginError && 'Wrong e-mail or password'}
-      </p>
     </form>
   );
 };
