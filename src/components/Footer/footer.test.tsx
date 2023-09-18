2180f2b43815d788d6feb4bf8d30a3ca92e666e9
Egor <itpd2010@yandex.ru>
2023-09-18 15:42:21 +0300

    feat: update footer test after changes

--- src/components/Footer/footer.test.tsx
+++ src/components/Footer/footer.test.tsx
@@ -1,72 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import { Footer } from './Footer';
-import teamMembersData from '../../pages/About/About.constants';
 
-describe('Footer component', () => {
-  it('should render the logo', () => {
-    const { getByAltText } = render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    );
+test('renders footer with correct content', () => {
+  render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  );
 
-    const logoElement = getByAltText('The Wonderful World');
-    expect(logoElement).toBeInTheDocument();
-  });
+  expect(screen.getByText('The Wonderful World ©')).toBeInTheDocument();
+  expect(screen.getByText('2023')).toBeInTheDocument();
 
-  it('should render navigation links', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    );
-
-    const mainLink = getByText('Main');
-    const catalogLink = getByText('Catalog');
-    const aboutLink = getByText('About us');
-
-    expect(mainLink).toBeInTheDocument();
-    expect(catalogLink).toBeInTheDocument();
-    expect(aboutLink).toBeInTheDocument();
-  });
-
-  it('should render GitHub links for team members', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    );
-
-    const githubLink1 = getByText(teamMembersData[0].name);
-    const githubLink2 = getByText(teamMembersData[1].name);
-    const githubLink3 = getByText(teamMembersData[2].name);
-
-    expect(githubLink1).toBeInTheDocument();
-    expect(githubLink2).toBeInTheDocument();
-    expect(githubLink3).toBeInTheDocument();
-  });
-
-  it('should render the RSSchool logo', () => {
-    const { getByTestId } = render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    );
-
-    const rsschoolLogo = getByTestId('rsSchoolLogo');
-    expect(rsschoolLogo).toBeInTheDocument();
-  });
-
-  it('should render the copyright year', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <Footer />
-      </MemoryRouter>
-    );
-
-    const copyrightText = getByText('© 2023');
-    expect(copyrightText).toBeInTheDocument();
-  });
+  const rsSchoolLink = screen.getByText('RS School');
+  expect(rsSchoolLink).toBeInTheDocument();
+  expect(rsSchoolLink).toHaveAttribute('target', '_blank');
 });
