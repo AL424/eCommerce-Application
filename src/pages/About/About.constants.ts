1ac365096b96ed23db4cc87357b9205d384c072b
Egor <itpd2010@yandex.ru>
2023-09-18 18:19:34 +0300

    feat: add real photo

--- src/pages/About/About.constants.ts
+++ src/pages/About/About.constants.ts
@@ -1,6 +1,6 @@
 import michaelScottImg from '../../assets/michaelscott.jpg';
 import jimHalpertImg from '../../assets/jimhalpert.jpg';
-import dwightschruteImg from '../../assets/dwightschrute.jpg';
+import egorImg from '../../assets/egorberezhnov.jpg';
 
 const teamMembersData = [
   {
@@ -21,7 +21,7 @@ const teamMembersData = [
     name: 'Egor Berezhnov',
     role: 'Frontend-Developer',
     bio: "Egor is the mastermind behind our coordinated efforts. He has successfully assembled and led our team to successful completion of the project. Egor ensures everyone is in sync, and helps coordinate our actions. His commitment to teamwork and communication has been a driving force in our project's success.",
-    img: dwightschruteImg,
+    img: egorImg,
     githubLink: 'https://github.com/ygrcore'
   }
 ];
