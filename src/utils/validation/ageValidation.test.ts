83bcbcd1d01c9e6445051eab6b3c0c45ff8fddfe
Egor <itpd2010@yandex.ru>
2023-09-18 12:00:49 +0300

    feat: ageValidation test

---
+++ src/utils/validation/ageValidation.test.ts
@@ -0,0 +1,27 @@
+import { ageValidation } from './ageValidation';
+
+describe('ageValidation', () => {
+  it('should return error message when age is less than 13', () => {
+    const value = '2020-01-01'; // Less than 13
+
+    const result = ageValidation.validate(value);
+
+    expect(result).toBe('You must be at least 13 years old');
+  });
+
+  it('should return true when age is 13 or older', () => {
+    const value = '2000-01-01'; // More than 13
+
+    const result = ageValidation.validate(value);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return undefined when value is empty', () => {
+    const value = undefined; // Empty value
+
+    const result = ageValidation.validate(value);
+
+    expect(result).toBeUndefined();
+  });
+});
